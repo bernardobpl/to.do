bc831a2f6bc2bf2ff01e5e1cfd99a45ca5faaed9
bernardobpl <bernardobpl97@gmail.com>
2021-09-19 12:58:28 -0300

    feat: manage tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,40 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  function isUniqueId(id: number){
+    return !tasks.some(task => task.id === id);
+  }
+
+  function generateId(): number{
+    const id = Math.floor(Math.random() * 999999);
+    return isUniqueId(id) ? id : generateId();
+  }
+
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return
+    
+    const newTask: Task = {
+      id: generateId(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks(oldTasks => [...oldTasks, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id);
+    setTasks(filteredTasks);
   }
 
   return (
